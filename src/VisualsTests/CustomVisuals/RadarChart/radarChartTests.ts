7e84a292281d4321d0e0c01d5c253dceaf45220a
Denis Kudelin <v-dekude@microsoft.com>
2016-11-01 15:12:41 +0300

    Fixed the radar chart test

--- src/VisualsTests/CustomVisuals/RadarChart/radarChartTests.ts
+++ src/VisualsTests/CustomVisuals/RadarChart/radarChartTests.ts
@@ -4,23 +4,30 @@ VisualConfig.
 describe("RadarChart", (reportUrl) => {
     beforeEach(() => {
         return browser
+            .waitForExist("iframe.visual-sandbox")
+            .element("iframe.visual-sandbox").then((res) => browser.frame(res.value))
             .waitForExist("svg.radarChart g.chartNode > *");
     });
 
-    itClient("selection test", function (done) {
-        var visual = new ClientVisuals.RadarChart();
-        ClientHelpers.clickElement(visual.chartDotsGrouped[0].eq(0));
-        ClientHelpers.clickElement(visual.chartDotsGrouped[0].eq(1), true);
+    it("selection test", () => {
+        return browser
+            .executeSpec(function() {
+                var visual = new ClientVisuals.RadarChart();
+                ClientHelpers.clickElement(visual.chartDotsGrouped[0].eq(0));
+                ClientHelpers.clickElement(visual.chartDotsGrouped[0].eq(1), true);
 
-        visual.chartDotsGrouped.forEach(dots => dots.toArray().map($).forEach((e,i) => {
-            if(i >= 2) {
-                expect(parseFloat(e.css('opacity'))).toBeLessThan(1);
-            } else {
-                expect(parseFloat(e.css('opacity'))).toBe(1);
-            }
-        }));
-
-        ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "14")
-            .then(done, done.fail);
+                visual.chartDotsGrouped.forEach(dots => dots.toArray().map($).forEach((e,i) => {
+                    if(i >= 2) {
+                        expect(parseFloat(e.css('opacity'))).toBeLessThan(1);
+                    } else {
+                        expect(parseFloat(e.css('opacity'))).toBe(1);
+                    }
+                }));
+            })
+            .frameParent()
+            .executeSpec(function (done) {
+                ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "14")
+                    .then(done, done.fail);
+            });
     });
 });
