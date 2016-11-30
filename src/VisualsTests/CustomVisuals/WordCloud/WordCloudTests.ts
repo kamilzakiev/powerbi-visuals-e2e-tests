a40aac06f3e7773972504728e39e3c3bf09eaac8
v-kazaki <v-kazaki@microsoft.com>
2016-11-30 13:21:47 +0300

    Fix for Word Cloud in sandbox

--- src/VisualsTests/CustomVisuals/WordCloud/WordCloudTests.ts
+++ src/VisualsTests/CustomVisuals/WordCloud/WordCloudTests.ts
@@ -1,27 +1,38 @@
 import {VisualConfig} from "../../../Common/exports";
 
 VisualConfig.
-describe("WordCloud", (reportUrl) => {
-    beforeEach(() => {
-        return browser
-            .waitForExist("svg.wordCloud g.words > g.word > *")
-            .pause(1000);
-    });
-
-    itClient("selection test", function (done) {
-        var visual = new ClientVisuals.WordCloud();
-        ClientHelpers.clickElement(visual.getWordRectByText("visualize"));
-        ClientHelpers.clickElement(visual.getWordRectByText("data"), true);
-
-        visual.wordRects.toArray().map(e => $(e).siblings()).forEach((e,i) => {
-            if(e.text() === "visualize" || e.text() === "data") {
-                expect(parseFloat(e.css('fill-opacity'))).toBe(1);
-            } else {
-                expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
-            }
+    describe("WordCloud", (reportUrl) => {
+        beforeEach(() => {
+            return browser
+                .waitForExist("iframe.visual-sandbox")
+                .element("iframe.visual-sandbox").then((res) => browser.frame(res.value))
+                .waitForExist("svg.wordCloud g.words > g.word > *")
+                .pause(1000);
         });
 
-        ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "6")
-            .then(done, done.fail);
+        it("selection test", () => {
+            return browser
+                .executeSpec(function () {
+                    var visual = new ClientVisuals.WordCloud();
+
+                    ClientHelpers.clickElement(visual.getWordRectByText("visualize"));
+                    ClientHelpers.clickElement(visual.getWordRectByText("data"), true);
+                })
+                .pause(1000)
+                .executeSpec(function () {
+                    var visual = new ClientVisuals.WordCloud();
+
+                    visual.wordRects.toArray().map(e => $(e).siblings()).forEach((e, i) => {
+                        if (e.text() === "visualize" || e.text() === "data") {
+                            expect(parseFloat(e.css('fill-opacity'))).toBe(1);
+                        } else {
+                            expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
+                        }
+                    });
+                })
+                .frameParent()
+                .executeSpec(function () {
+                    expect(ClientHelpers.getTextWithoutChild($("svg.card > g > text.value"))).toBe("6");
+                });
+        });
     });
-});
