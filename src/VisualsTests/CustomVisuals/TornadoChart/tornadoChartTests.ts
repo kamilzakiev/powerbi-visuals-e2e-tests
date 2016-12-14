ec9ec3b2740fc1ec2a3ce573fde94bad0c2e126b
v-kazaki <v-kazaki@microsoft.com>
2016-12-14 16:09:46 +0300

    Tornado moved to iframe

--- src/VisualsTests/CustomVisuals/TornadoChart/tornadoChartTests.ts
+++ src/VisualsTests/CustomVisuals/TornadoChart/tornadoChartTests.ts
@@ -4,24 +4,31 @@ VisualConfig.
 describe("TornadoChart", (reportUrl) => {
     beforeEach(() => {
         return browser
+            .waitForExist("iframe.visual-sandbox")
+            .element("iframe.visual-sandbox").then((res) => browser.frame(res.value))
             .waitForExist("svg.tornado-chart g.columns > *");
     });
 
-    itClient("selection test", function (done) {
-        var visual = new ClientVisuals.TornadoChart();
+    it("selection test", () => {
+        return browser
+            .executeSpec(function () {
+                var visual = new ClientVisuals.TornadoChart();
 
-        ClientHelpers.clickElement(visual.columns.eq(0));
-        ClientHelpers.clickElement(visual.columns.eq(1), true);
+                ClientHelpers.clickElement(visual.columns.eq(0));
+                ClientHelpers.clickElement(visual.columns.eq(1), true);
 
-        visual.columns.toArray().map($).forEach((e,i) => {
-            if(i % (visual.columns.length/2) >= 2) {
-                expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
-            } else {
-                expect(parseFloat(e.css('fill-opacity'))).toBe(1);
-            }
-        });
-
-        ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "82.40K")
-            .then(done, done.fail);
+                visual.columns.toArray().map($).forEach((e, i) => {
+                    if (i % (visual.columns.length / 2) >= 2) {
+                        expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
+                    } else {
+                        expect(parseFloat(e.css('fill-opacity'))).toBe(1);
+                    }
+                });
+            })
+            .frameParent()
+            .executeSpec(function (done) {
+                ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "82.40K")
+                    .then(done, done.fail);
+            });
     });
 });
