60d33a2a33bbe8e15fb1e99ebf6b56f8ae3c481e
v-kazaki <v-kazaki@microsoft.com>
2016-12-14 16:06:23 +0300

    Sunburst moved to iframe

--- src/VisualsTests/CustomVisuals/Sunburst/sunburstTests.ts
+++ src/VisualsTests/CustomVisuals/Sunburst/sunburstTests.ts
@@ -4,28 +4,30 @@ VisualConfig.
 describe("Sunburst", (reportUrl) => {
     beforeEach(() => {
         return browser
+            .waitForExist("iframe.visual-sandbox")
+            .element("iframe.visual-sandbox").then((res) => browser.frame(res.value))
             .waitForExist("svg.mainDrawArea g.container > *");
     });
 
-    itClient("selection test", function (done) {
-        var visual = new ClientVisuals.Sunburst();
+    it("selection test", () => {
+        return browser
+            .executeSpec(function () {
+                var visual = new ClientVisuals.Sunburst();
 
-        if(jsCommon.BrowserUtils.isFirefox()) {
-            done();
-            return;
-        };
+                ClientHelpers.dispatchMouseEvent("mousedown", visual.visibleNodes.eq(0));
 
-        ClientHelpers.dispatchMouseEvent("mousedown", visual.visibleNodes.eq(0));
-
-        visual.visibleNodes.toArray().map($).forEach((e,i) => {
-            if(i >= 1) {
-                expect(parseFloat(e.css('opacity'))).toBeLessThan(1);
-            } else {
-                expect(parseFloat(e.css('opacity'))).toBe(1);
-            }
-        });
-
-        ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "33")
-            .then(done, done.fail);
+                visual.visibleNodes.toArray().map($).forEach((e, i) => {
+                    if (i >= 1) {
+                        expect(parseFloat(e.css('opacity'))).toBeLessThan(1);
+                    } else {
+                        expect(parseFloat(e.css('opacity'))).toBe(1);
+                    }
+                });
+            })
+            .frameParent()
+            .executeSpec(function (done) {
+                ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "33")
+                    .then(done, done.fail);
+            });
     });
 });
