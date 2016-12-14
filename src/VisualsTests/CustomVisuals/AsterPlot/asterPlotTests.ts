ff986bb6a3ba04c7c19afbb6f3fb0ab6578e1fb9
v-kazaki <v-kazaki@microsoft.com>
2016-12-14 14:34:58 +0300

    Aster Plot moved to iframe

--- src/VisualsTests/CustomVisuals/AsterPlot/asterPlotTests.ts
+++ src/VisualsTests/CustomVisuals/AsterPlot/asterPlotTests.ts
@@ -4,24 +4,31 @@ VisualConfig.
 describe("AsterPlot", (reportUrl) => {
     beforeEach(() => {
         return browser
+            .waitForExist("iframe.visual-sandbox")
+            .element("iframe.visual-sandbox").then((res) => browser.frame(res.value))
             .waitForExist("svg.asterPlot g.asterSlices > path.asterSlice");
     });
 
-    itClient("selection test", function (done) {
-        var visual = new ClientVisuals.AsterPlot();
+    it("selection test", () => {
+        return browser
+            .executeSpec(function () {
+                var visual = new ClientVisuals.AsterPlot();
 
-        ClientHelpers.clickElement(visual.slices.eq(0));
-        ClientHelpers.clickElement(visual.slices.eq(1), true);
+                ClientHelpers.clickElement(visual.slices.eq(0));
+                ClientHelpers.clickElement(visual.slices.eq(1), true);
 
-        visual.slices.toArray().map($).forEach((e,i) => {
-            if(i >= 2) {
-                expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
-            } else {
-                expect(parseFloat(e.css('fill-opacity'))).toBe(1);
-            }
-        });
-
-        ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "2.47K")
-            .then(done, done.fail);
+                visual.slices.toArray().map($).forEach((e, i) => {
+                    if (i >= 2) {
+                        expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
+                    } else {
+                        expect(parseFloat(e.css('fill-opacity'))).toBe(1);
+                    }
+                });
+            })
+            .frameParent()
+            .executeSpec(function (done) {
+                ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "2.47K")
+                    .then(done, done.fail);
+            });
     });
 });
