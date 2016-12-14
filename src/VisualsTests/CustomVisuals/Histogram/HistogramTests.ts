1d27b1ab154e594a05632f6f4463e6383611d67c
v-kazaki <v-kazaki@microsoft.com>
2016-12-14 14:42:50 +0300

    Histogram moved to iframe

--- src/VisualsTests/CustomVisuals/Histogram/HistogramTests.ts
+++ src/VisualsTests/CustomVisuals/Histogram/HistogramTests.ts
@@ -4,22 +4,29 @@ VisualConfig.
 describe("Histogram", (reportUrl) => {
     beforeEach(() => {
         return browser
+            .waitForExist("iframe.visual-sandbox")
+            .element("iframe.visual-sandbox").then((res) => browser.frame(res.value))
             .waitForExist("svg.histogram g.columns > *");
     });
 
-    itClient("selection test", function (done) {
-        var visual = new ClientVisuals.Histogram();
-        ClientHelpers.clickElement(visual.columnRects.eq(0), true);
+    it("selection test", () => {
+        return browser
+            .executeSpec(function () {
+                var visual = new ClientVisuals.Histogram();
+                ClientHelpers.clickElement(visual.columnRects.eq(0), true);
 
-        visual.columnRects.toArray().map($).forEach((e,i) => {
-            if(i >= 1) {
-                expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
-            } else {
-                expect(parseFloat(e.css('fill-opacity'))).toBe(1);
-            }
-        });
-
-        ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "7")
-            .then(done, done.fail);
+                visual.columnRects.toArray().map($).forEach((e, i) => {
+                    if (i >= 1) {
+                        expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
+                    } else {
+                        expect(parseFloat(e.css('fill-opacity'))).toBe(1);
+                    }
+                });
+            })
+            .frameParent()
+            .executeSpec(function (done) {
+                ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "7")
+                    .then(done, done.fail);
+            });
     });
 });
