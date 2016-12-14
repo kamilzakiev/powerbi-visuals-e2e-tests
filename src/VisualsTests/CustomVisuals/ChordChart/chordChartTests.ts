29e356cd989b5345e07a98cda88a683128647689
v-kazaki <v-kazaki@microsoft.com>
2016-12-14 14:38:49 +0300

    Chord Chart moved to iframe

--- src/VisualsTests/CustomVisuals/ChordChart/chordChartTests.ts
+++ src/VisualsTests/CustomVisuals/ChordChart/chordChartTests.ts
@@ -4,22 +4,29 @@ VisualConfig.
 describe("ChordChart", (reportUrl) => {
     beforeEach(() => {
         return browser
+            .waitForExist("iframe.visual-sandbox")
+            .element("iframe.visual-sandbox").then((res) => browser.frame(res.value))
             .waitForExist("svg.chordChart g.slices > *");
     });
 
-    itClient("selection test", function (done) {
-        var visual = new ClientVisuals.ChordChart();
-        ClientHelpers.clickElement(visual.slices.eq(0));
+    it("selection test", () => {
+        return browser
+            .executeSpec(function () {
+                var visual = new ClientVisuals.ChordChart();
+                ClientHelpers.clickElement(visual.slices.eq(0));
 
-        visual.slices.toArray().map($).forEach((e,i) => {
-            if(i >= 1) {
-                expect(parseFloat(e.css('opacity'))).toBeLessThan(1);
-            } else {
-                expect(parseFloat(e.css('opacity'))).toBe(1);
-            }
-        });
-
-        ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "5")
-            .then(done, done.fail);
+                visual.slices.toArray().map($).forEach((e, i) => {
+                    if (i >= 1) {
+                        expect(parseFloat(e.css('opacity'))).toBeLessThan(1);
+                    } else {
+                        expect(parseFloat(e.css('opacity'))).toBe(1);
+                    }
+                });
+            })
+            .frameParent()
+            .executeSpec(function (done) {
+                ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "5")
+                    .then(done, done.fail);
+            });
     });
 });
