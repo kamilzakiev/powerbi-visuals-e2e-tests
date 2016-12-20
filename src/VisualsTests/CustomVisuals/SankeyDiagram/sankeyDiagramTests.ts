2034b3d31d6cc732437cf148196df51cbf342e51
v-kazaki <v-kazaki@microsoft.com>
2016-12-20 14:30:34 +0300

    Sankey moved to iframe

--- src/VisualsTests/CustomVisuals/SankeyDiagram/sankeyDiagramTests.ts
+++ src/VisualsTests/CustomVisuals/SankeyDiagram/sankeyDiagramTests.ts
@@ -4,23 +4,30 @@ VisualConfig.
 describe("SankeyDiagram", (reportUrl) => {
     beforeEach(() => {
         return browser
+            .waitForExist("iframe.visual-sandbox")
+            .element("iframe.visual-sandbox").then((res) => browser.frame(res.value))
             .waitForExist("svg.sankeyDiagram g.links > *");
     });
 
-    itClient("selection test", function (done) {
-        var visual = new ClientVisuals.SankeyDiagram();
-        ClientHelpers.clickElement(visual.linkElements.eq(0));
-        ClientHelpers.clickElement(visual.linkElements.eq(1), true);
+    it("selection test", () => {
+        return browser
+            .executeSpec(function () {
+                var visual = new ClientVisuals.SankeyDiagram();
+                ClientHelpers.clickElement(visual.linkElements.eq(0));
+                ClientHelpers.clickElement(visual.linkElements.eq(1), true);
 
-        visual.linkElements.toArray().map($).forEach((e,i) => {
-            if(i >= 2) {
-                expect(e.is(".selected")).toBeFalsy();
-            } else {
-                expect(e.is(".selected")).toBeTruthy();
-            }
-        });
-
-        ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "9")
-            .then(done, done.fail);
+                visual.linkElements.toArray().map($).forEach((e, i) => {
+                    if (i >= 2) {
+                        expect(e.is(".selected")).toBeFalsy();
+                    } else {
+                        expect(e.is(".selected")).toBeTruthy();
+                    }
+                });
+            })
+            .frameParent()
+            .executeSpec(function (done) {
+                ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "9")
+                    .then(done, done.fail);
+            });
     });
 });
