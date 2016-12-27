536f1acae7a0093f2e709d3f648ad7885bb13317
v-kazaki <v-kazaki@microsoft.com>
2016-12-27 13:16:42 +0300

    Gantt visual moved to iframe

--- src/VisualsTests/CustomVisuals/Gantt/ganttTests.ts
+++ src/VisualsTests/CustomVisuals/Gantt/ganttTests.ts
@@ -4,23 +4,30 @@ VisualConfig.
 describe("Gantt", (reportUrl) => {
     beforeEach(() => {
         return browser
+            .waitForExist("iframe.visual-sandbox")
+            .element("iframe.visual-sandbox").then((res) => browser.frame(res.value))
             .waitForExist("div.gantt-body g.task-group > g.task > *");
     });
 
-    itClient("selection test", function (done) {
-        var visual = new ClientVisuals.Gantt();
-        ClientHelpers.clickElement(visual.tasks.eq(0));
-        ClientHelpers.clickElement(visual.tasks.eq(1), true);
+    it("selection test", () => {
+        return browser
+            .executeSpec(function () {
+                var visual = new ClientVisuals.Gantt();
+                ClientHelpers.clickElement(visual.tasks.eq(0));
+                ClientHelpers.clickElement(visual.tasks.eq(1), true);
 
-        visual.tasks.toArray().map($).forEach((e,i) => {
-            if(i >= 2) {
-                expect(parseFloat(e.css('opacity'))).toBeLessThan(1);
-            } else {
-                expect(parseFloat(e.css('opacity'))).toBe(1);
-            }
-        });
-
-        ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "9")
-            .then(done, done.fail);
+                visual.tasks.toArray().map($).forEach((e, i) => {
+                    if (i >= 2) {
+                        expect(parseFloat(e.css('opacity'))).toBeLessThan(1);
+                    } else {
+                        expect(parseFloat(e.css('opacity'))).toBe(1);
+                    }
+                });
+            })
+            .frameParent()
+            .executeSpec(function (done) {
+                ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "9")
+                    .then(done, done.fail);
+            });
     });
 });
