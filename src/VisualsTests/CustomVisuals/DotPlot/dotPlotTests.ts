126ee35d8e1a66d035f05eab3bcea65508f7591a
v-kazaki <v-kazaki@microsoft.com>
2017-01-17 16:55:19 +0300

    Dot Plot moved to Iframe

--- src/VisualsTests/CustomVisuals/DotPlot/dotPlotTests.ts
+++ src/VisualsTests/CustomVisuals/DotPlot/dotPlotTests.ts
@@ -4,23 +4,33 @@ VisualConfig.
 describe("DotPlot", (reportUrl) => {
     beforeEach(() => {
         return browser
+            .waitForExist("iframe.visual-sandbox")
+            .element("iframe.visual-sandbox").then((res) => browser.frame(res.value))
             .waitForExist("svg.dotplot g.dotplotGroup > *");
     });
 
-    itClient("selection test", function (done) {
-        var visual = new ClientVisuals.DotPlot();
-        ClientHelpers.clickElement(visual.dotGroups.eq(0));
-        ClientHelpers.clickElement(visual.dotGroups.eq(1), true);
+    it("selection test", () => {
+        return browser
+            .executeSpec(function () {
+                var visual = new ClientVisuals.DotPlot();
+                ClientHelpers.clickElement(visual.dotGroups.eq(0));
+                ClientHelpers.clickElement(visual.dotGroups.eq(1), true);
 
-        visual.dotGroups.toArray().map($).forEach((e,i) => {
-            if(i >= 2) {
-                expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
-            } else {
-                expect(parseFloat(e.css('fill-opacity'))).toBe(1);
-            }
-        });
-
-        ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "$152K")
-            .then(done, done.fail);
+                visual.dotGroups.toArray().map($).forEach((e, i) => {
+                    if (i >= 2) {
+                        expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
+                    } else {
+                        expect(parseFloat(e.css('fill-opacity'))).toBe(1);
+                    }
+                });
+            })
+            .frameParent()
+            .executeSpec(function (done) {
+                ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "$152K")
+                    .then(done, done.fail);
+            });
     });
 });
+
+
+
