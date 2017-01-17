551a905222c866bfa1ccd5db2c8c422be336beca
v-kazaki <v-kazaki@microsoft.com>
2017-01-17 16:39:48 +0300

    Bullet chart moved to iframe

--- src/VisualsTests/CustomVisuals/BulletChart/bulletChartTests.ts
+++ src/VisualsTests/CustomVisuals/BulletChart/bulletChartTests.ts
@@ -4,24 +4,31 @@ VisualConfig.
 describe("BulletChart", (reportUrl) => {
     beforeEach(() => {
         return browser
+            .waitForExist("iframe.visual-sandbox")
+            .element("iframe.visual-sandbox").then((res) => browser.frame(res.value))
             .waitForExist("div.bulletChart rect.range");
-    });
+    });    
 
-    itClient("selection test", function (done) {
-        var visual = new ClientVisuals.BulletChart();
+    it("selection test", () => {
+        return browser
+            .executeSpec(function () {
+                var visual = new ClientVisuals.BulletChart();
 
-        ClientHelpers.clickElement(visual.rangeRectsGrouped[0].first());
-        ClientHelpers.clickElement(visual.rangeRectsGrouped[1].first(), true);
+                ClientHelpers.clickElement(visual.rangeRectsGrouped[0].first());
+                ClientHelpers.clickElement(visual.rangeRectsGrouped[1].first(), true);
 
-        visual.rangeRectsGrouped.map(e => e.first()).forEach((e,i) => {
-            if(i >= 2) {
-                expect(parseFloat(e.css('opacity'))).toBeLessThan(1);
-            } else {
-                expect(parseFloat(e.css('opacity'))).toBe(1);
-            }
-        });
-
-        ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "222K")
-            .then(done, done.fail);
+                visual.rangeRectsGrouped.map(e => e.first()).forEach((e, i) => {
+                    if (i >= 2) {
+                        expect(parseFloat(e.css('opacity'))).toBeLessThan(1);
+                    } else {
+                        expect(parseFloat(e.css('opacity'))).toBe(1);
+                    }
+                });
+            })
+            .frameParent()
+            .executeSpec(function (done) {
+                ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "222K")
+                    .then(done, done.fail);
+            });
     });
 });
