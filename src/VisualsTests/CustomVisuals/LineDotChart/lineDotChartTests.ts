7aebdf8320d48509f04fb2f6a4820d952399ca78
v-kazaki <v-kazaki@microsoft.com>
2017-01-24 16:54:16 +0300

    LineDot Chart moved to iframe

--- src/VisualsTests/CustomVisuals/LineDotChart/lineDotChartTests.ts
+++ src/VisualsTests/CustomVisuals/LineDotChart/lineDotChartTests.ts
@@ -4,23 +4,29 @@ VisualConfig.
 describe("LineDotChart", (reportUrl) => {
     beforeEach(() => {
         return browser
+            .waitForExist("iframe.visual-sandbox")
+            .element("iframe.visual-sandbox").then((res) => browser.frame(res.value))
             .waitForExist("svg.lineDotChart g.line circle.point");
     });
 
-    itClient("selection test", function (done) {
-        var visual = new ClientVisuals.LineDotChart();
-        ClientHelpers.clickElement(visual.dots.eq(0));
+    it("selection test", () => {
+        return browser
+            .executeSpec(function () {
+                var visual = new ClientVisuals.LineDotChart();
+                ClientHelpers.clickElement(visual.dots.eq(0));
 
-        setTimeout(() => {
-            visual.dots.toArray().map($).forEach((e,i) => {
-                if(i >= 1) {
-                    expect(e).not.toHaveCss({"opacity":"1"});
-                } else {
-                    expect(e).toHaveCss({"opacity":"1"});
-                }
+                visual.dots.toArray().map($).forEach((e, i) => {
+                    if (i >= 1) {
+                        expect(e).not.toHaveCss({ "opacity": "1" });
+                    } else {
+                        expect(e).toHaveCss({ "opacity": "1" });
+                    }
+                });
+            })
+            .frameParent()
+            .executeSpec(function (done) {
+                ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "1")
+                    .then(done, done.fail);
             });
-            ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "1")
-            .then(done, done.fail);
-        }, 5000);
     });
 });
