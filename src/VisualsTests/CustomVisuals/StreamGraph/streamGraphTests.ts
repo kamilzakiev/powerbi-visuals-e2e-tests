b7fc74a149a97b0f8d1853a4575c8fb5b4fedf91
v-kazaki <v-kazaki@microsoft.com>
2017-02-09 17:15:23 +0300

    Stream Graph moved to sandbox

--- src/VisualsTests/CustomVisuals/StreamGraph/streamGraphTests.ts
+++ src/VisualsTests/CustomVisuals/StreamGraph/streamGraphTests.ts
@@ -4,23 +4,30 @@ VisualConfig.
 describe("StreamGraph", (reportUrl) => {
     beforeEach(() => {
         return browser
+            .waitForExist("iframe.visual-sandbox")
+            .element("iframe.visual-sandbox").then((res) => browser.frame(res.value))
             .waitForExist("svg.streamGraph g.dataPointsContainer > *");
     });
 
-    itClient("selection test", function (done) {
-        var visual = new ClientVisuals.StreamGraph();
-        ClientHelpers.clickElement(visual.layers.eq(0));
-        ClientHelpers.clickElement(visual.layers.eq(1), true);
+    it("selection test", () => {
+        return browser
+            .executeSpec(function () {
+                var visual = new ClientVisuals.StreamGraph();
+                ClientHelpers.clickElement(visual.layers.eq(0));
+                ClientHelpers.clickElement(visual.layers.eq(1), true);
 
-        visual.layers.toArray().map($).forEach((e,i) => {
-            if(i >= 2) {
-                expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
-            } else {
-                expect(parseFloat(e.css('fill-opacity'))).toBe(1);
-            }
-        });
-
-        ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "191.60")
-            .then(done, done.fail);
+                visual.layers.toArray().map($).forEach((e, i) => {
+                    if (i >= 2) {
+                        expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
+                    } else {
+                        expect(parseFloat(e.css('fill-opacity'))).toBe(1);
+                    }
+                });
+            })
+            .frameParent()
+            .executeSpec(function (done) {
+                ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "191.60")
+                    .then(done, done.fail);
+            });
     });
 });
