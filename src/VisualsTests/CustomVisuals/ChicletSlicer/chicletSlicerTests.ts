fc705ed55c593b6f4dadb871663d151d04baf7b6
v-kazaki <v-kazaki@microsoft.com>
2017-03-16 16:15:07 -0700

    Chiclet Slicer moved to iframe

--- src/VisualsTests/CustomVisuals/ChicletSlicer/chicletSlicerTests.ts
+++ src/VisualsTests/CustomVisuals/ChicletSlicer/chicletSlicerTests.ts
@@ -4,21 +4,28 @@ VisualConfig.
 describe("ChicletSlicer", (reportUrl) => {
     beforeEach(() => {
         return browser
+            .waitForExist("iframe.visual-sandbox")
+            .element("iframe.visual-sandbox").then((res) => browser.frame(res.value))
             .waitForExist("div.chicletSlicer div.cell > *");
     });
 
-    itClient("selection test", function (done) {
-        var visual = new ClientVisuals.ChicletSlicer();
-        var unselectedColor = visual.slicerItemContainers.eq(0).css('background-color');
+    it("selection test", () => {
+        return browser
+            .executeSpec(function () {
+                var visual = new ClientVisuals.ChicletSlicer();
+                var unselectedColor = visual.slicerItemContainers.eq(0).css('background-color');
 
-        ClientHelpers.clickElement(visual.slicerItemContainers.eq(0), true);
-        ClientHelpers.clickElement(visual.slicerItemContainers.eq(1), true);
+                ClientHelpers.clickElement(visual.slicerItemContainers.eq(0), true);
+                ClientHelpers.clickElement(visual.slicerItemContainers.eq(1), true);
 
-        visual.slicerItemContainers.toArray().map($).forEach((e,i) => {
-            ClientHelpers.assertColorsMatch(e.css('background-color'), unselectedColor, i < 2);
-        });
-
-        ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "$159M")
-            .then(done, done.fail);
+                visual.slicerItemContainers.toArray().map($).forEach((e, i) => {
+                    ClientHelpers.assertColorsMatch(e.css('background-color'), unselectedColor, i < 2);
+                });
+            })
+            .frameParent()
+            .executeSpec(function (done) {
+                ClientHelpers.waitUntil(() => ClientHelpers.getTextWithoutChild($("svg.card > g > text.value")) === "$159M")
+                    .then(done, done.fail);
+            });
     });
 });
